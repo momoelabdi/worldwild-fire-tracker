3a4fec2756d495913695debeff3b953e693cc722
Mohammed <mohammed@Geraldines-iMac.local>
2022-11-18 21:07:45 +0100

    fixed api issues

--- src/Maps.js
+++ src/Maps.js
@@ -11,19 +11,22 @@ import { pink } from '@mui/material/colors';
 
 const Maps = ({ eventData }) => {
   let api_key = process.env.REACT_APP_WATER_MAP_API_KEY;
+
+
   const markers = eventData.map((ev, index) => {
     if (ev.categories[0].id === 8) {
       return (
-        <LocalFireDepartmentIcon sx={{ color: pink[500] }}   key={index}
-          latitude={ev.geometries[0].coordinates[1]}
-          longitude={ev.geometries[0].coordinates[0]}
-        />
+        <Marker longitude={ev.geometries[0].coordinates[0]} latitude={ev.geometries[0].coordinates[1]}  key={index} anchor="bottom">
+          <LocalFireDepartmentIcon sx={{ color: pink[500] }}/>
+      </Marker>
+       
       );
     }
     return null;
   });
+// console.log(markers.coordinates[0]);
+// console.log(...markers.geometries);
 
-console.log(...markers);
   return (
     <Map
       initialViewState={{
@@ -36,10 +39,7 @@ console.log(...markers);
       mapboxAccessToken={api_key}
       mapStyle="mapbox://styles/mapbox/streets-v9"
     >
-      <Marker longitude={100} latitude={40} anchor="bottom">
-
-          <LocalFireDepartmentIcon sx={{ color: pink[500] }}/>
-      </Marker>
+      {markers}
     </Map>
   );
 };
