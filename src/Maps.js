777e5a17f54e8e260c0da79592f78a53eede8a13
Mohammed <mohammed@Geraldines-iMac.local>
2022-11-22 20:19:47 +0100

    ttp

--- src/Maps.js
+++ src/Maps.js
@@ -1,54 +1,45 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 import Map, { Marker } from "react-map-gl";
 import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
 import { pink } from "@mui/material/colors";
-import LocationInfoBox from "./LocationInfoBox";
+import { Tooltip } from "@mui/material";
+
+
 
 const Maps = ({ eventData }) => {
   let api_key = process.env.REACT_APP_WATER_MAP_API_KEY;
 
   const [locationInfo, setLocationInfo] = useState(null);
-  const [isShown, setIsShown] = useState(false);
-
+  
   const markers = eventData.map((ev, index) => {
     if (ev.categories[0].id === 8) {
       return (
         <Marker
-          longitude={ev.geometries[0].coordinates[0]}
-          latitude={ev.geometries[0].coordinates[1]}
-          key={index}
-          anchor="bottom"
+        longitude={ev.geometries[0].coordinates[0]}
+        latitude={ev.geometries[0].coordinates[1]}
+        key={index}
+        anchor="bottom"
         >
+          <Tooltip title={ev.title} id={ev.id} date={ev.geometries[0].date} placement="top-start" >
           <LocalFireDepartmentIcon
             sx={{ color: pink[500] }}
-            onMouseEnter={() =>
-              setIsShown(true) &
-              setLocationInfo({
-                id: ev.id,
-                title: ev.title,
-                date: ev.geometries[0].date,
-              })
-            }
-            onMouseLeave={() => setIsShown(false)}
-          />
+            />
+          </Tooltip>
         </Marker>
       );
     }
     return null;
   });
 
-  console.log( eventData );
-
   const content = eventData.map((e, i) => {
-   
     return (
       <ul key={i} className="item">
-        <li >
+        <li>
           ID : <strong>{e.id}</strong>
         </li>
-        <li  >
+        <li>
           Title : <strong>{e.title}</strong>
         </li>
         <li>
@@ -58,14 +49,11 @@ const Maps = ({ eventData }) => {
     );
   });
 
-
   return (
     <div>
       <div className="sidebar">
         <h1>SideBar</h1>
-        <div className="listings">
-       {content}
-       </div>  
+        <div className="listings">{content}</div>
       </div>
 
       <div className="map">
@@ -83,10 +71,6 @@ const Maps = ({ eventData }) => {
           {markers}
         </Map>
       </div>
-
-      {isShown
-        ? locationInfo && <LocationInfoBox info={locationInfo} />
-        : !isShown}
     </div>
   );
 };
