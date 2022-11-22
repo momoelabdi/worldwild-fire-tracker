440308dfca34a650cb23f056efee8a08a862b4ca
Mohammed <mohammed@Geraldines-iMac.local>
2022-11-22 18:05:51 +0100

    lists style

--- src/Maps.js
+++ src/Maps.js
@@ -1,35 +1,25 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 import Map, { Marker } from "react-map-gl";
 import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
 import { pink } from "@mui/material/colors";
 import LocationInfoBox from "./LocationInfoBox";
-import SideBar from "./SideBar";
-import { logDOM } from "@testing-library/react";
 
 const Maps = ({ eventData }) => {
   let api_key = process.env.REACT_APP_WATER_MAP_API_KEY;
 
   const [locationInfo, setLocationInfo] = useState(null);
   const [isShown, setIsShown] = useState(false);
-  let [sideBarInfo, setSideBarInfo] = useState(null);
 
   const markers = eventData.map((ev, index) => {
     if (ev.categories[0].id === 8) {
       return (
-        // setSideBarInfo({
-        //   key: ev.index,
-        //   id: ev.id,
-        //   title: ev.title,
-        //   date: ev.geometries[0].date,
-        // }) &&
         <Marker
           longitude={ev.geometries[0].coordinates[0]}
           latitude={ev.geometries[0].coordinates[1]}
           key={index}
           anchor="bottom"
-          // onClick={() =>  setSideBarInfo({ id: ev.id, title: ev.title, date: ev.geometries[0].date})}
         >
           <LocalFireDepartmentIcon
             sx={{ color: pink[500] }}
@@ -52,28 +42,30 @@ const Maps = ({ eventData }) => {
   console.log( eventData );
 
   const content = eventData.map((e, i) => {
+   
     return (
-      <ul>
-        <li key={i}>
+      <ul key={i} className="item">
+        <li >
           ID : <strong>{e.id}</strong>
         </li>
-        <li  key={i}>
+        <li  >
           Title : <strong>{e.title}</strong>
         </li>
-        <li  key={i}>
+        <li>
           Date : <strong>{e.geometries[0].date}</strong>
         </li>
       </ul>
     );
   });
 
-  // console.log(content);
 
   return (
     <div>
       <div className="sidebar">
         <h1>SideBar</h1>
-       {content}  
+        <div className="listings">
+       {content}
+       </div>  
       </div>
 
       <div className="map">
