9ac69732c7389906450468f3e6ce1139ab943d08
Mohammed <mohammed@Geraldines-iMac.local>
2022-11-23 23:16:03 +0100

    flyTo

--- src/Maps.js
+++ src/Maps.js
@@ -1,86 +1,111 @@
 import { useState } from "react";
 import "./App.css";
 import "mapbox-gl/dist/mapbox-gl.css";
-import Map, { Marker } from "react-map-gl";
+import { Map, Marker, useMap } from "react-map-gl";
 import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
 import { pink } from "@mui/material/colors";
-import { Tooltip } from "@mui/material";
-
-
+import LocationInfoBox from "./LocationInfoBox";
 
 const Maps = ({ eventData }) => {
   let api_key = process.env.REACT_APP_WATER_MAP_API_KEY;
 
-  
+  const [locationInfo, setLocationInfo] = useState(null);
+  const [isShown, setIsShown] = useState(false);
+  const wildFires = 8;
+
   const markers = eventData.map((ev, index) => {
-    if (ev.categories[0].id === 8) {
+    if (ev.categories[0].id === wildFires) {
       return (
         <Marker
-        longitude={ev.geometries[0].coordinates[0]}
-        latitude={ev.geometries[0].coordinates[1]}
-        key={index}
-        anchor="bottom"
+          longitude={ev.geometries[0].coordinates[0]}
+          latitude={ev.geometries[0].coordinates[1]}
+          key={index}
+          anchor="bottom"
         >
-          <Tooltip title={ev.title} id={ev.id} date={ev.geometries[0].date} placement="top-start" >
           <LocalFireDepartmentIcon
             sx={{ color: pink[500] }}
-            />
-          </Tooltip>
+            onMouseEnter={() =>
+              setIsShown(true) &
+              setLocationInfo({
+                id: ev.id,
+                title: ev.title,
+                date: ev.geometries[0].date,
+              })
+            }
+            onMouseLeave={() => setIsShown(false)}
+          />
         </Marker>
       );
     }
     return null;
   });
 
-  const handleClick = (eventData, e) => {
-    console.log(e.id, eventData);
+  function NavigateButton() {
+    const {current: map} = useMap();
+    const onClick = () => {
+      map.flyTo({latitude: 13.3414, longitude: 47.332});
+    };
+
+    return <button onClick={onClick}>Go</button>;
   }
 
-//  console.log(markers)
-
   const content = eventData.map((e, i) => {
-    return (
-      <ul key={i}  onClick={handleClick} className="item">
-        <li>
-          ID : <strong>{e.id}</strong>
-        </li>
-        <li>
-          Title : <strong>{e.title}</strong>
-        </li>
-        <li>
-          Date : <strong>{e.geometries[0].date}</strong>
-        </li>
-      </ul>
-    );
+    if (e.categories[0].id === wildFires) {
+      return (
+        <ul key={i} className="item">
+          <li>
+            ID : <strong>{e.id}</strong>
+          </li>
+          <li>
+            Title : <strong>{e.title}</strong>
+          </li>
+          <li>
+            Date : <strong>{e.geometries[0].date.slice(0, 10)}</strong>
+          </li>
+          <li>
+            Lat: <strong>{e.geometries[0].coordinates[0]}</strong>
+          </li>
+          <li>
+            Lng: <strong>{e.geometries[0].coordinates[1]}</strong>
+          </li>
+        </ul>
+      );
+    }
+    return null;
   });
-
-  //  console.log(markers);
-  //  console.log(content);
-
-
-
 
   return (
     <div>
       <div className="sidebar">
         <h1>SideBar</h1>
-        <div className="listings" >{content}</div>
+        <NavigateButton />
+        <div className="listings">
+          {content}
+        </div>
       </div>
 
       <div className="map">
-        <Map
+      <Map className="myMap"
           initialViewState={{
-            longitude:  13.3414 ,
-            latitude: 47.3320 ,
+            longitude: 13.3414,
+            latitude: 47.332,
             zoom: 3,
+            // pitch: 0,
+            // bearing: 0,
+            // duration: 12000, // Animate over 12 seconds
+            essential: true,
             // projection: "globe",
           }}
+          
           // style={{ width: 800, height: 800 }}
           mapboxAccessToken={api_key}
           mapStyle="mapbox://styles/mapbox/streets-v9"
         >
           {markers}
         </Map>
+        {isShown
+          ? locationInfo && <LocationInfoBox info={locationInfo} />
+          : !isShown}
       </div>
     </div>
   );
