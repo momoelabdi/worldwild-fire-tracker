9c20481bd380a7cb6b6cf32eda25c454318b9816
Mohammed El Abdi <momo.elabdi@bluewin.ch>
2022-12-03 19:16:29 +0100

    typo

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className="header">
       <h1>
-        <LocalFireDepartmentIcon sx={{ color: pink[500] }} /> World wild Fires
+        <LocalFireDepartmentIcon sx={{ color: pink[500] }} /> World Wild Fires
         Tracker
       </h1>
     </header>
